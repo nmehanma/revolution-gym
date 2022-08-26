3d262ca5f23c6dfa57a0f07c948cfc81a139e40a
nmehanma <nmehanma@gmail.com>
2022-08-26 16:54:47 -0400

    introduce different pages that are to be used

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, facilis eos? Ex omnis, ut eligendi magni dignissimos voluptatem reiciendis. Beatae, ratione consectetur dolore aut illum cumque doloribus dolorem et ducimus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, facilis eos? Ex omnis, ut eligendi magni dignissimos voluptatem reiciendis. Beatae, ratione consectetur dolore aut illum cumque doloribus dolorem et ducimus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, facilis eos? Ex omnis, ut eligendi magni dignissimos voluptatem reiciendis. Beatae, ratione consectetur dolore aut illum cumque doloribus dolorem et ducimus.</p>
+      
+    </div>
+  )
+}
---
+++ src/pages/recreational.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function recreational() {
+  return (
+    <div>recreational</div>
+  )
+}
