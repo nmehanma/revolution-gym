566a69a1d9997ae23aab96c01851f3072bc16809
nmehanma <nmehanma@gmail.com>
2022-08-26 18:59:42 -0400

    introduce font-awesome and use facebook, instagram, and envelope icons

--- src/pages/components/Footer/Footer.js
+++ src/pages/components/Footer/Footer.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import { IconName } from "react-icons/fa";
+import React from "react"
+import { FaFacebookF, FaInstagram, FaEnvelope } from "react-icons/fa"
 
 export default function Footer() {
   return (
     <footer>
-        <p>Copyright 2022 Revolution Gymnastics</p>
-        <a href="mailto:info@revolutiongym.ca" title="Go Email">
-          <p>Email</p>
-        </a>
-      </footer>
+      <p>Copyright 2022 Revolution Gymnastics</p>
+      <a href="https://www.facebook.com/RevGymCanada">
+        <FaFacebookF />
+      </a>
+      <a href="https://www.instagram.com/revgymcanada/">
+        <FaInstagram />
+      </a>
+      <a href="mailto:info@revolutiongym.ca">
+        <FaEnvelope />
+      </a>
+      
+    </footer>
   )
 }
