971e998b44dfbc6f92a0e3ec33ad6acf95b4c125
nmehanma <nmehanma@gmail.com>
2022-08-26 17:52:02 -0400

    introduce initial phase of css styling to Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "./components/Layout"
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h1>Hello World</h1>
+          <h2>Develop and Deploy</h2>
+          <p>Revolution website new</p>
+          <Link className={styles.btn} to="/about">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
