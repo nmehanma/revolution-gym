2db7b5ec4390747b0a591a981adb17557ceebaeb
nmehanma <nmehanma@gmail.com>
2022-08-27 15:44:00 -0400

    introduce clinic file

---
+++ src/pages/about/clinic.js
@@ -0,0 +1,122 @@
+import React from "react"
+import Layout from "../components/Layout"
+import aaronImage from "../../../public/images/clinic/aaron.jpeg"
+
+export default function clinic() {
+  return (
+    <Layout>
+      <div>
+        <h1>Clinics & Guest Speaking</h1>
+        <img src={aaronImage} alt=""></img>
+        <p>
+          Aaron Brokenshire offers clinics both in and out of house and speaking
+          engagements throughout Canada. Please E-mail for more information on
+          booking and rates.
+        </p>
+        <h4>Clinic Topics Include:</h4>
+        <p>
+          "Keys to Creating and Running a thriving gymnastics business"
+          "Everything you need to know about twisting backwards on FX - 1/2 to 3
+          1/2" "The Yurchenko vault" "More repetition without injury - Safety
+          and injury prevention through modified apparatus training" "Physical
+          preparation for higher level gymnastics" "Spotting 101" "Advanced
+          spotting techniques" And Much More!
+        </p>
+        <h4>Recent Clinics:</h4>
+        <p>
+          Gymnastics Ontario Congress Oct. '11 (Employed by Gymnastics Ontario)
+          Pre Novice National Training Camp, Edmonton, AB - Sept. '11 (Employed
+          by Alberta Gymnastics Federation) Woodward Gymnastics Camp,
+          Pennsylvania - July '11 - Staff Coach and Clinician Pre Novice
+          National Training Camp, Laval, QC - March 2011 - Tumbling Clinician
+          Canadian Adventure Camp - NCCP Level 3 Guest
+        </p>
+      </div>
+      <div>
+        <h1>What people are saying!</h1>
+        <p>
+          Aaron arrived in Calgary energized and ready to work with our Alberta
+          athletes and coaches! The gymnasts responded very well to his new
+          ideas and coaching style. I loved his ‘developing grit’ session.
+          Myself and my athletes left the camp motivated to work harder and
+          reach our goals. Thank you so much Aaron for sharing your passion and
+          expertise with us.
+        </p>
+
+        <p>
+          Kristi Cloman Women’s Program Director Capital City Gymnastics
+          Edmonton, AB
+        </p>
+
+        <p>
+          “Gymnastics Saskatchewan had Aaron down for a Team Training Camp with
+          our National & HP athletes in the Summer of 2018. He was attentive,
+          prepared and worked with the athletes in a positive and supportive
+          manner. The drills and technique suggestions he made to coaches helped
+          us focus on areas of weakness and build on our strengths. We would
+          highly suggest him to work with other club or provincial teams and
+          would definitely have him back for future camps.”
+        </p>
+
+        <p>Leigh Stoddart ​ HP Coach for Gymnastics Saskatchewan</p>
+
+        <p>
+          Aaron and Angela's session on Keys to Creating / Running a Thriving
+          Gymnastics Business at the 2011 Gymnastics Ontario Congress was an
+          absolute success. They are truly professionals in their field and
+          participants had the opportunity to learn what makes a Gymnastics
+          Business successful. It is amazing that as clinicians they are so
+          eager and willing to share their 'secrets of success' to the general
+          community for the betterment of our sport. We appreciated their
+          candour and professionalism and will eagerly invite them to Congress
+          again.
+        </p>
+
+        <p>Sean Holmes, Technical Director at Gymnastics Ontario</p>
+
+        <p>
+          "Aaron Brokenshire recently came to Alberta to be an event presenter
+          at our annual Pre Novice Training Camp. He ran two training sessions
+          in the gym, and one classroom session. All of our coaches in
+          attendance really valued his creative ideas and vast experience to
+          draw from. His expertise on bars, and in particular, his suggestions
+          for a weekly training plan for HP athletes, was appreciated by our
+          coaches due to its easy application in their own gyms. His friendly
+          and efficient manner with the athletes, and his approachable manner
+          with the coaches made him an excellent addition to our camp."
+        </p>
+        <p>
+          Krista Morris, Women's Program Coordinator, Alberta Gymnastics
+          Federation
+        </p>
+        <p>
+          "Aaron Brokenshire has been an instructor and clinician at Canadian
+          Adventure Camp for a number of years. He is an expert clinician for
+          the National Coaching Certification courses which we hold at the end
+          of August each summer and his presentations involve adult coaches and
+          child demonstrators. The residential nature of our camp means that I
+          have a unique opportunity to observe staff at work and at play. Aaron
+          is extremely professional at all times in his contact with my clients
+          and with the youngsters who are demonstrating. He has wonderful
+          charisma and an easy manner, making him very popular with all ages in
+          camp. Aaron consistently achieves the highest possible ratings on our
+          survey of customers at the end of each course. I cannot recommend him
+          highly enough and feel very fortunate that he will again be teaching
+          courses with us this coming summer."
+        </p>
+        <p>F.B. (Skip) Connett, Director, Canadian Adventure Camp</p>
+        <p>
+          "It was great to have coach Aaron Brokenshire come to Woodward Camp
+          with a group of his gymnasts. His uneven bar clinic showed natural
+          progressions that were well thought out and explained. The most
+          telling part of the clinic was how in tune his gymnasts were with what
+          Aaron was talking about in regards to the progressions and corrections
+          he was giving/discussing. Having Aaron and his athletes at camp this
+          summer added a great deal to Woodward Camp for the staff, visiting
+          coaches, and campers! I hope they'll be back soon!"
+        </p>
+        <p>Craig Ballard (Assistant Gymnastics Coach, Kent State University)</p>
+      </div>
+    </Layout>
+  )
+}
