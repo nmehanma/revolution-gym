afed98001acf2a548e6bec961b4efdbc04f278fa
nmehanma <nmehanma@gmail.com>
2022-08-31 14:59:57 -0400

    create toddler.js file

--- src/pages/recreational/toddler.js
+++ src/pages/recreational/toddler.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react"
+import Layout from "../components/Layout"
 
 export default function toddler() {
   return (
-    <div>toddler</div>
+    <Layout>
+      <div>
+        <h1>Toddler Programs (18 months - 3.5 years)</h1>
+        <h3>Parent & Tot – Ages 18 months to 2.5 years (co-ed)</h3>
+        <p>
+          This 40 minute class offers a great introduction to various types of
+          movement and provides an excellent way to begin instilling positive
+          learning attitudes. As we know, the formative years are the most
+          important to a child’s growth and development. With this interactive
+          program, parents will begin to expose their children to healthy
+          learning skills that will last a lifetime. This program runs for 9
+          weeks.
+        </p>
+        <h3>Tumble Tots - Ages 2.5 years to 3.5 years (co-ed)</h3>
+        <p>
+          This 40 minute class offers a great introduction to various types of
+          movement and provides an excellent way to begin instilling positive
+          learning attitudes. As we know, the formative years are the most
+          important to a child’s growth and development. With this interactive
+          program, parents will begin to expose their children to healthy
+          learning skills that will last a lifetime. This program runs for 9
+          weeks.
+        </p>
+
+        <h2>
+          <a href="https://revolutiongym.uplifterinc.com/registration/" target="_blank" >Ready to register? Click Here!</a>
+        </h2>
+      </div>
+    </Layout>
   )
 }
